3f09ac623efe3bbdfb09b204e982fa3783f77f73
Orvar Segerström <orvarsegerstrom@gmail.com>
2017-12-17 07:02:50 +0100

    Day 17: p1

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,5 +1,3 @@
-//use std::collections::LinkedList;
-
 fn main() {
     const STEP_SIZE: usize = 377;
 
@@ -8,14 +6,16 @@ fn main() {
     memory.push(0);
     let mut position = 0;
     let mut size = 1;
-    
+
     for i in 1.. {
         position = (position + STEP_SIZE) % size;
-        memory.insert(position, i);
+        memory.insert(position + 1, i);
+        position = position + 1;
         size = size + 1;
 
         if i == 2017 {
-            println!("{}", memory[(position + 1) % size]);
+            println!("memory[pos]:   {}", memory[(position + 0) % size]);
+            println!("memory[pos+1]: {}", memory[(position + 1) % size]);
             break;
         }
     }
