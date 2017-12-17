d896b1789b240cec6f20091369b67de02cda9166
Orvar Segerström <orvarsegerstrom@gmail.com>
2017-12-17 07:07:32 +0100

    Day 17: p2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,22 +1,45 @@
 fn main() {
     const STEP_SIZE: usize = 377;
 
-    //let mut list: LinkedList<u32> = LinkedList::new();
-    let mut memory: Vec<usize> = Vec::new();
-    memory.push(0);
-    let mut position = 0;
-    let mut size = 1;
+    {
+        let mut memory: Vec<usize> = Vec::new();
+        memory.push(0);
+        let mut position = 0;
+        let mut size = 1;
 
-    for i in 1.. {
-        position = (position + STEP_SIZE) % size;
-        memory.insert(position + 1, i);
-        position = position + 1;
-        size = size + 1;
+        for i in 1.. {
+            position = (position + STEP_SIZE) % size;
+            memory.insert(position + 1, i);
+            position = position + 1;
+            size = size + 1;
 
-        if i == 2017 {
-            println!("memory[pos]:   {}", memory[(position + 0) % size]);
-            println!("memory[pos+1]: {}", memory[(position + 1) % size]);
-            break;
+            if i == 2017 {
+                println!("memory[pos]:   {}", memory[(position + 0) % size]);
+                println!("memory[pos+1]: {}", memory[(position + 1) % size]);
+                break;
+            }
+        }
+    }
+    
+    {
+        let mut zero_position = 0;
+        let mut value_after_zero = 0;
+        let mut position = 0;
+        let mut size = 1;
+        for i in 1.. {
+            position = (position + STEP_SIZE) % size;
+            if position == zero_position {
+                value_after_zero = i;
+            } else if position < zero_position {
+                zero_position += 1;
+            }
+            position = position + 1;
+            size = size + 1;
+
+            if i == 50_000_000 {
+                println!("value_after_zero: {}", value_after_zero);
+                break;
+            }
         }
     }
 }
